d0ad7ccb21ce937506ab06101a454032e378411a
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-01-02 12:17:15 +0200

    feat: add and style Welcome component

---
+++ src/components/Welcome.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container welcome">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">{{title}}</h2>
+        <form @submit.prevent="enterChat">
+          <label for="name">Enter your name:</label>
+          <input type="text" name="name" v-model="name">
+          <button class="btn teal">Enter Chat</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      title: "Real Time Chat App",
+      name: null
+    };
+  },
+  methods: {
+    enterChat() {
+      console.log(this.name);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.welcome {
+  max-width: 400px;
+  margin-top: 100px;
+}
+
+.welcome h2 {
+  font-size: 3em;
+}
+
+.welcome button {
+  margin: 30px;
+}
+</style>
